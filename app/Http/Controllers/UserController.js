cfadb7f3c3a1c8db1ab0cd5528ff28e17d3fa965
Derrick Bray <derrickbray@yahoo.com>
2016-11-09 21:11:12 -0600

    user controller store, and alert html

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -12,7 +12,26 @@ class UserController {
 
   * store(request, response) {
     //
+    const { username, email, password } = request.all();
 
+    try {
+      const user = yield User.create({ username, email, password: yield Hash.make(password) });
+
+      yield request.auth.login(user);
+
+      yield request.with({
+        success: 'You have successfully registered with ribbeter!',
+      }).flash();
+
+      response.redirect('/users');
+    } catch (e) {
+      yield request
+      .withOut('password')
+      .andWith({ error: 'That username or email has already been used.' }).flash();
+
+      response.redirect('back');
+    }
+    response.send(user);
   }
 
 }
