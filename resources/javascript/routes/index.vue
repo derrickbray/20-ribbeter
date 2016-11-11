e9f60c2eb6c42e1da026463d0f24f03b35104f19
Derrick Bray <derrickbray@yahoo.com>
2016-11-11 13:34:04 -0600

    style on app

--- resources/javascript/routes/index.vue
+++ resources/javascript/routes/index.vue
@@ -1,22 +1,31 @@
 <template lang="html">
   <div class="">
 
-    <div class="">
+    <h1 class="page-title">Ribbits</h1>
 
-      <div class="register-card">
+    <div class="main">
+
+      <div class="ribbit-card">
         <div class="card-container">
 
           <div class="card__head">
             <h2>New Ribbits</h2>
           </div>
+          <div class="user__card-text">
+            <div class="user__card-title">What's going on?</div>
+            <textarea v-model="content" rows="10" class="ribbit-textarea"></textarea>
 
+          </div>
 
-
+          <div class="button">
+            <a href="/register" class="btn-white">Sign Up</a>
+            {{ form.submit('app', null, {class: 'btn-green'}) }}
+          </div>
 
         </div>
       </div>
 
-      <div class="register-card">
+      <div class="ribbit-card ribbits">
         <div class="card-container">
 
           <div class="card__head">
